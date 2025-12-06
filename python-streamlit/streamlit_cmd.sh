f54e51dd0b9905d0ac6d8894c54a4183d010d857
Ajay Kidave <ajayvk@clace.io>
2025-12-05 16:50:09 -0800

    Remove heredoc usage in streamlit spec since kaniko does not support it

---
+++ python-streamlit/streamlit_cmd.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+run_args="run ${app_file:-streamlit_app.py}"
+
+if [[ "${app_file:-}" = "hello" ]]; then
+  run_args="hello"
+fi
+
+exec streamlit $run_args \
+  --server.address=0.0.0.0 \
+  --server.baseUrlPath="${CL_APP_PATH:-/}"
